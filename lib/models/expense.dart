abc47fe9ca6738de5877b776b37883c5e061f369
M9heb <123528241+M9heb@users.noreply.github.com>
2025-03-23 14:57:49 +0430

    Adds category section to expenses

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,14 +2,25 @@ import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
 
+enum Category {
+  food,
+  family,
+  travel,
+  leisure,
+  work,
+  education,
+}
+
 class Expense {
   Expense({
     required this.amount,
     required this.title,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
